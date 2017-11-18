8bcbc69f2e5e676405755002492963e4235bf7c9
Schmid-Lorenz <schmid.lorenz@outlook.com>
2017-11-18 23:10:43 +0100

    First try dynamically allocating storage to array

---
+++ Triangles.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+int elements = 1;
+int* array = NULL;
+array = malloc(elements * sizeof(int));
+
+
+if (array == NULL)
+{
+printf("Allocating memory was not possible\n");
+return 1;
+}
+
+for (int counter = 0; counter < elements; counter++)
+{
+  array[counter] = counter *counter;
+  printf("%3i\n", array[counter]);
+}
+
+free(array);
+
+return 0;
+}
